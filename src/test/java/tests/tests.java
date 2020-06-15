88e3c112ccec82ae2e013d7b59352757032af0c7
NikSav777 <nikitasavin32@gmail.com>
2020-06-15 23:47:08 +0400

    ex. 2

---
+++ src/test/java/tests/tests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class tests {
+    @Test
+    public void FirstTest()
+    {
+        System.out.println("1");
+    }
+    @Test
+    public void SecondTest()
+    {
+        System.out.println("2");
+    }
+
+}
